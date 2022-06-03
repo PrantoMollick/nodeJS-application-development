5c5a4f0179d0b02843dc9a63c8735f0d92c45e8b
Pranto Mollick <prantocse01@gmail.com>
2022-06-03 21:15:17 +0600

    parsing the incoming request

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.use('/add-product', (req, res, next) => {
-  res.send("<h1>Here is Add Product page</h1>");
+  res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="submit" value="Add Product" /> </form>');
+});
+
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log("Is another middleware");
   res.send('<h1>Hello express</h1>')
 }); 
 
