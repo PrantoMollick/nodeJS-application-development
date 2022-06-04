9eec3fc1e61098e81ab2f3756d66a8fa06c77360
Pranto Mollick <prantocse01@gmail.com>
2022-06-04 13:11:33 +0600

    adding 404 page using use middleware

--- app.js
+++ app.js
@@ -3,24 +3,21 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(shopRoutes);
+app.use(adminRoutes);
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="submit" value="Add Product" /> </form>');
-});
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not found </h1>');
+})
 
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello express</h1>')
-}); 
 
 app.listen(3000);
 
