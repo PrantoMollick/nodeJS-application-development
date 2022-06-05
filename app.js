4a646e164156c418e0aad777cc0810cbb3c41faa
Pranto Mollick <prantocse01@gmail.com>
2022-06-05 21:11:33 +0600

    working with ejs

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const http = require('http');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
-
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 const app = express();
 
-const handlebars = exphbs.create({extname: '.hbs', layoutsDir: 'views/layouts', defaultLayout: 'main-layout'});
-app.engine('hbs', handlebars.engine);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
