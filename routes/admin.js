1ffa7530368728fdf8f3516e67c3e2c44ba12a54
Pranto Mollick <prantocse01@gmail.com>
2022-06-06 14:59:33 +0600

    implement of MVC pattern

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,14 @@
-const path = require('path');
 const express = require('express');
-
-const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const products = [];
+const productsController = require('../controllers/products');
+
 
 // /admin/add-product => Get request
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    productCSS: true,
-    formCSS: true,
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => Post request
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
