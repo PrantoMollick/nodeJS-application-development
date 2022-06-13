6796bf9be6584ab74d47e73636734039241d76cd
Pranto Mollick <prantocse01@gmail.com>
2022-06-13 14:20:31 +0600

    working on the product model to edit our product

--- controllers/admin.js
+++ controllers/admin.js
@@ -27,51 +27,49 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit === "true";
-//   if (!editMode) {
-//     return res.status(301).redirect("/");
-//   }
-//   const prodId = req.params.productId;
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit === "true";
+  if (!editMode) {
+    return res.status(301).redirect("/");
+  }
+  const prodId = req.params.productId;
 
-//   req.user
-//     .getProducts({ where: { id: prodId } })
-//     // Product.findByPk(prodId)
-//     .then((products) => {
-//       const product = products[0];
-//       if (!product) {
-//         return res.redirect("/");
-//       }
-//       res.render("admin/edit-product", {
-//         pageTitle: "Edit Product",
-//         path: "/admin/edit-product",
-//         editing: editMode,
-//         product: product,
-//       });
-//     })
-//     .catch((err) => console.log(error));
-// };
+  Product.findById(prodId)
+    .then((product) => {
+      console.log(product);
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(error));
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   const updatedTitle = req.body.title;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedPrice = req.body.price;
-//   const updatedDescription = req.body.description;
-//   Product.findByPk(prodId)
-//     .then((product) => {
-//       product.title = updatedTitle;
-//       product.price = updatedPrice;
-//       product.description = updatedDescription;
-//       product.imageUrl = updatedImageUrl;
-//       product.save();
-//     })
-//     .then((result) => {
-//       console.log("Updated Product");
-//       res.redirect("/admin/products");
-//     })
-//     .catch((err) => console.log(err));
-// };  
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      product.imageUrl = updatedImageUrl;
+      product.save();
+    })
+    .then((result) => {
+      console.log("Updated Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};  
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
