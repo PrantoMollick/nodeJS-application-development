0a81cba1455b2c1fe9ac47726f6becbe6501cacc
Pranto Mollick <prantocse01@gmail.com>
2022-06-23 03:04:45 +0600

    initialize authentication startup

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById("62b22e85584057067113ae71")
     .then((user) => {
@@ -21,6 +29,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
