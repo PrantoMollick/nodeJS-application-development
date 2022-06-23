2a2ccc36dd385253ca86a296f7ee64b1c84ac2b7
Pranto Mollick <prantocse01@gmail.com>
2022-06-23 12:40:57 +0600

    fixing the order button

--- controllers/shop.js
+++ controllers/shop.js
@@ -95,7 +95,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.email,
+          email: req.user.email,
           userId: req.user,
         },
         products: products,
