b8e741e0db64e794092008be5df0697e1e786f7d
Pranto Mollick <prantocse01@gmail.com>
2022-06-24 19:32:21 +0600

    adding logic to update the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -164,3 +164,54 @@ exports.postReset = (req, res, next) => {
     });
 
 };
+
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      if(!user) {
+        req.flash('error', "token is expired")
+        return res.redirect('/reset');
+      }
+      let messageFlash = req.flash("error");
+      message = messageFlash.length > 0 ? messageFlash[0] : null;
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        isAuthenticated: false,
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      // console.log(user);
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => console.log(err));
+};
