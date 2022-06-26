9cc32d4be404500a1a2eb113eac9841ddd2ede91
Pranto Mollick <prantocse01@gmail.com>
2022-06-26 14:47:16 +0600

    sanitizing user data

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,7 +24,12 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: false,
-    errorMessage: message
+    errorMessage: message, 
+    oldInput: {
+      email: '', 
+      password: ''
+    }, 
+    validationErrors: []
   });
 };
 
@@ -54,15 +59,28 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render('auth/login', {
       path: '/login', 
       pageTitle: 'Login',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg, 
+      oldInput: {
+        email: email, 
+        password: password
+      }, 
+      validationErrors: errors.array()
     });
   }
 
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash('error', 'Invalid email or password!');
-        return res.redirect("/login");
+        return res.status(422).render('auth/login', {
+          path: '/login', 
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.', 
+          oldInput: {
+            email: email, 
+            password: password
+          }, 
+          validationErrors: []
+        });
       }
 
       bcrypt
@@ -76,8 +94,16 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          req.flash('error', 'Invalid email or password!');
-          res.redirect("/login");
+          return res.status(422).render('auth/login', {
+            path: '/login', 
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password.', 
+            oldInput: {
+              email: email, 
+              password: password
+            }, 
+            validationErrors: []
+          });
         })
         .catch((err) => {
           console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { check, body } = require('express-validator');
+const { check, body } = require("express-validator");
 
-const User = require('../models/user');
+const User = require("../models/user");
 const authController = require("../controllers/auth");
 
 const router = express.Router();
@@ -13,10 +13,14 @@ router.get("/signup", authController.getSignup);
 router.post(
   "/login",
   [
-    body("email").isEmail().withMessage("Please enter a valid email address!"),
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email address!")
+      .normalizeEmail(),
     body("password", "Password has to be valid.")
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(),
   ],
   authController.postLogin
 );
@@ -31,21 +35,22 @@ router.post(
         return User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
             return Promise.reject(
-                "E-mail exists already, please pick a different one."
+              "E-mail exists already, please pick a different one."
             );
           }
         });
-      }),
-    body("password")
+      })
+      .normalizeEmail(),
+
+    body(
+      "password",
+      "Please enter a password with only number and text and at least 5 characters."
+    )
       .isLength({ min: 5 })
-      .withMessage(
-        "Please enter a password with only number and text and at least 5 characters."
-      )
       .isAlphanumeric()
-      .withMessage(
-        "Please enter a password only numbers and text and at least 5 characters."
-      ),
-    body("confirmPassword").custom((value, { req }) => {
+      .trim(),
+
+    body("confirmPassword").trim().custom((value, { req }) => {
       if (value !== req.body.password) {
         throw new Error("Password have to match");
       }
@@ -55,14 +60,14 @@ router.post(
   authController.postSignup
 );
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
 
-router.get('/reset', authController.getReset);
+router.get("/reset", authController.getReset);
 
-router.post('/reset', authController.postReset);
+router.post("/reset", authController.postReset);
 
-router.get('/reset/:token', authController.getNewPassword);
+router.get("/reset/:token", authController.getNewPassword);
 
-router.post('/new-password', authController.postNewPassword);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
